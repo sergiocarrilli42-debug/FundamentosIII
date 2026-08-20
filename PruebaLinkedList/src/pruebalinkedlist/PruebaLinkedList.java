856323a4234ab737508b9b12cb7be93f4d485e0d
Sergipato <sergiocarrilli42@gmail.com>
2026-08-20 12:49:16 -0700

    commit inicial

---
+++ PruebaLinkedList/src/pruebalinkedlist/PruebaLinkedList.java
@@ -0,0 +1,46 @@
+
+package pruebalinkedlist;
+/*
+    @Sergio Carrillo Osorio
+    @17/08/2026
+*/
+class Student {
+    String name;
+    int score;
+    Student next;
+    public Student(String name, int score){
+        this.name = name;
+        this.score = score;
+        this.next = null;
+    }
+}
+class LnkdLst{
+    Student head;
+    public LnkdLst()//Constructor
+    {
+        this.head = null;
+    }
+    //Agrega un nuevo estudiante al final de la lista
+    public void add(String name, int score){
+    Student newStudent = new Student(name,score);
+    if (head == null){
+        head = newStudent;
+        return;
+    }
+    Student current = head;
+    while (current.next !=null){
+        current = current.next;
+    }
+    current.next = newStudent;
+  }
+}
+public class PruebaLinkedList {
+
+    public static void main(String[] args) {
+        LnkdLst lista = new LnkdLst();
+        lista.add("Linus", 90);
+        lista.add("Ada", 95);
+        lista.add("Paul", 78);
+    }
+    
+}
