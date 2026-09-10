ffddacc10093f74137350c542102f9a4de593d21
Sergipato <sergiocarrilli42@gmail.com>
2026-09-10 12:48:52 -0700

    Added a new proyect

---
+++ Rover/src/rover/Rover.java
@@ -0,0 +1,59 @@
+
+package rover;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Queue;
+
+public class Rover {
+
+
+    public static void main(String[] args) {
+        String cadena = args.length > 0 ? args[0] : "path1.txt";
+        ArrayList<String> lineas = leerArchivo(cadena);
+        Queue<String> queue = addToQueue(lineas);
+        travelQueue(queue);
+    }
+        public static ArrayList<String> leerArchivo(String nombreArchivo) {
+        ArrayList<String> lineas = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(nombreArchivo))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                lineas.add(linea);
+            }
+        } catch (IOException e) {
+            System.err.println("Error al leer el archivo: " + e.getMessage());
+        }
+        return lineas;
+    }
+    public static void travelQueue(Queue <String> queue){
+        while(!queue.isEmpty()){
+            String linea = queue.poll();//Sacar un elemento del inicio de la cola
+            if (linea != null){
+                if(linea.toUpperCase().equals("A")){
+                    System.out.print("^");
+                }
+                else if(linea.toUpperCase().equals("D")){
+                    System.out.print("v");
+                }
+                else if (linea.toUpperCase().equals("S")){
+                    System.out.print("-");
+                }
+            }
+            
+        }
+        System.out.println();
+    }
+    public static Queue<String> addToQueue(ArrayList<String>lineas){
+        Queue<String> queue = new java.util.LinkedList<>();
+        for (String linea : lineas){
+            queue.add(linea);
+        }
+        return queue;
+    }
+}
+    
+    
+
